2d660c7dbedaf956e23601f8807866332c4e92ca
Kartik Verma <71791933+bugverma@users.noreply.github.com>
2026-03-07 23:43:53 +0530

    Update harry.py

--- harry.py
+++ harry.py
@@ -2,6 +2,7 @@ import boto3
 
 AWS_ACCESS_KEY_ID="AKIA34UWIKTBTWQPHBZP"
 AWS_SECRET_ACCESS_KEY="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"
+## AWS_SECRET_ACCESS_KEY="8Fy3swUo37IMv5AyRtdoRDNiOe7qiIicUym0Dy4A"
 
 session = boto3.Session(
     aws_access_key_id=AWS_ACCESS_KEY_ID,
