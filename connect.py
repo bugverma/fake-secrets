fc889c57b55946de9764b03811dfb4ba7afdcc43
Kartik Verma <71791933+bugverma@users.noreply.github.com>
2026-03-08 18:41:04 +0530

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,7 +1,7 @@
 import boto3
-
-AWS_ACCESS_KEY_ID="AKIAZ7TBUSG3OB3KGBN4"
-AWS_SECRET_ACCESS_KEY="go5jR9Vh3nxmHVcQosHyJ8A1C3fBUyz8elQ3dDj9"
+# Redacted for Privacy Reasons
+AWS_ACCESS_KEY_ID=""
+AWS_SECRET_ACCESS_KEY=""
 
 session = boto3.Session(
     aws_access_key_id=AWS_ACCESS_KEY_ID,
